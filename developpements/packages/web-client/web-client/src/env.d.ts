7e9f4f9c1fbdd836ee4a8840396bbde2d1baacd6
joubijuba <juba_ramdani@hotmail.com>
2023-04-20 11:29:59 +0200

    removed wrong folder

--- developpements/packages/web-client/web-client/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    API_ORIGIN: string;
-    NODE_ENV: string;
-    VUE_ROUTER_BASE: string | undefined;
-    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
-  }
-}
